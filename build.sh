298b9bb0a33dbeb6e05875576bb7a4c3acce8ef5
John Kamenik <jkamenik@fourv.com>
2016-11-26 13:49:21 -0500

    Adding a build script.

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/." && pwd )"
+DIR="$1"
+set -e
+
+if [ -z "$DIR" ]; then
+  DIR=$APP_DIR
+fi
+
+cd $DIR
+
+# have to clean up the DIR
+DIR=$(pwd)
+
+if [ -f "./Dockerfile" ]; then
+  echo "=== Docker file found in $DIR"
+  TAG=$(basename $DIR)
+  IMAGE=$(dirname $DIR | sed -e "s|^${APP_DIR}/||")
+
+  echo "TAG: $TAG"
+  echo "IMAGE: $IMAGE"
+
+  echo "=== Building ${IMAGE}:${TAG}"
+
+  docker build -t "${IMAGE}:${TAG}" .
+  docker push "${IMAGE}:${TAG}"
+fi
+
+dirs=$(find -L . -type d -depth 1 | grep -v .git)
+echo "=== No Docker file, recursing: $dirs"
+for path in $dirs; do
+  $APP_DIR/build.sh $DIR/$path
+done
