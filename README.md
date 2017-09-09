75f62c729095db66014827b62c1f7695e7d5b8ce
John Kamenik <jkamenik@fourv.com>
2017-09-08 20:17:37 -0400

    Update to check for existing images.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # docker-images
-A collection of my public docker images.
+
+A collection of my public docker images that get pushed to Dockerhub.
+
+## Build
+
+```
+$ docker login
+$ git clone git@github.com:jkamenik/docker-images.git
+$ cd docker-images
+$ build.sh
+```
+
+This will build any non-existing images and push them to Dockerhub.
--- build.sh
+++ build.sh
@@ -13,13 +13,40 @@ cd $DIR
 # have to clean up the DIR
 DIR=$(pwd)
 
+function exists_in_repo() {
+  if [ -z "${1}" -o -z "$2" ]; then
+    return 1
+  fi
+  
+  local name=$1
+  local tag=$2
+  
+  echo $name $tag
+  
+  # works because these are public
+  local tags=$(curl --progress-bar "https://hub.docker.com/v2/repositories/${name}/tags/${tag}/" 2>/dev/null | tr -s '[:upper:]' '[:lower:]')
+  echo $tags
+  if [[ "${tags}" =~ 'not found' ]]; then
+    return 1
+  fi
+  
+  return 0
+}
+
 if [ -f "./Dockerfile" ]; then
   echo "=== Docker file found in $DIR"
   TAG=$(basename $DIR)
   IMAGE=$(dirname $DIR | sed -e "s|^${APP_DIR}/||")
-
-  echo "TAG: $TAG"
-  echo "IMAGE: $IMAGE"
+  
+  if [ -z "$FORCE_BUILD" ]; then
+    echo "=== Checking if ${IMAGE}:${TAG} exist"
+    
+    if exists_in_repo "$IMAGE" "$TAG" ; then
+      echo "--- existing image found, skipping"
+      echo "--- use FORCE_BUILD=true to force a build"
+      exit 0
+    fi
+  fi
 
   echo "=== Building ${IMAGE}:${TAG}"
 
